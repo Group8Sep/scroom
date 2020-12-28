1d4f84a8942c2704238d6f1fab602c5af83fe02a
Documentation Builder <project@mandryn.org>
2020-12-28 19:38:19 +0000

    Automated documentation build for branch code-analysis, changeset 834fb6d6e1b9457c650162d787f3ea172e806e64.

---
+++ code-analysis/navtreedata.js
@@ -0,0 +1,57 @@
+var NAVTREE =
+[
+  [ "Scroom", "index.html", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classColormappable.html#a65f7747cb8c21704aed2bcbb05401f8b",
+"classOperations24bpp.html#a3a16af1f5fc66b2f35f8e7667ccb3a8d",
+"classScroom_1_1Bitmap_1_1SampleIterator.html#a103bb667ba5dce21a10941904a8cc4a1",
+"classScroom_1_1Utils_1_1Detail_1_1ProgressStore.html#ad64f01bbb18a7b887d33584e5f1ac94e",
+"classThreadPool.html",
+"classTiledBitmapViewData.html#a86cf1099b453666642cbc60a1a2b41d3",
+"colormap-helpers_8cc_source.html",
+"local_8hh.html#ae2de07f0f9ff468e76d12f302807d3d8",
+"tiledbitmapviewdata_8cc.html#af32b1dddcb044cfdf445db8f34409874"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ code-analysis/structSelection.js
@@ -0,0 +1,13 @@
+var structSelection =
+[
+    [ "ConstPtr", "structSelection.html#a818de2f80ecb351760f69345132c1569", null ],
+    [ "Ptr", "structSelection.html#a0c872a61d8b40bd2195a71a27fadb7b0", null ],
+    [ "Selection", "structSelection.html#a3fa57dfb27cea617496736b9a479cbb0", null ],
+    [ "Selection", "structSelection.html#a1f46687a759cf302f2cdb9314858f501", null ],
+    [ "endsAt", "structSelection.html#a99b4855c3c68bc847fb926c31faed223", null ],
+    [ "height", "structSelection.html#ab8dcdca670a48bac885b0444e030ad3e", null ],
+    [ "length", "structSelection.html#a0f3962d1a01cb237a38b6fe0efcd778e", null ],
+    [ "width", "structSelection.html#a9dda8fd17c23fbc88b51011660cf58e1", null ],
+    [ "end", "structSelection.html#aa9f0d2ba632840298b9c04fd68393a79", null ],
+    [ "start", "structSelection.html#ae2ead14497cf20f1beb80318ca95da4b", null ]
+];
